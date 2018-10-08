ad50cc86a9e6293501e70bc37ff8fac5e65c02d8
julia98 <925583000@qq.com>
2018-10-09 00:59:23 +1100

    fix

--- a3/checkout.php
+++ a3/checkout.php
@@ -92,8 +92,8 @@
                 </div>
                 <div class="six columns">
                     <label for="exampleEmailInput">Credit Card</label>
-                    <input class="u-full-width" type="text" name="credit_card">
-                    <img src="images/visa.png" id="visaIcon" style="display:none" oninput="validate_cardNumber()" />
+                    <input class="u-full-width" type="text" name="credit_card" oninput="validate_cardNumber()">
+                    <img src="images/visa.png" id="visaIcon" style="display:none" />
                 </div>
                 <div class="six columns">
                     <label for="exampleEmailInput">Expiry Date</label>
--- a3/js/checkout.js
+++ a3/js/checkout.js
@@ -3,10 +3,10 @@ function validate_cardNumber() {
     cardNumber = cardNumber.trim();
     alert(cardNumber);
     if(cardNumber[0] == "4" && cardNumber.length >= 13 && cardNumber.length <= 16) {
-        document.getElementById("visaIcon").style = "display=block";
+        document.getElementById("visaIcon").style = "display:block";
         return true;
     } else {
-        document.getElementById("visaIcon").style = "display=none";
+        document.getElementById("visaIcon").style = "display:none";
         return false;
     }
 }
