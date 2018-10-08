c89c5a40917d3489096b6216259da556311c85f7
julia98 <925583000@qq.com>
2018-10-08 20:52:04 +1100

    fix

--- a3/products.php
+++ a3/products.php
@@ -59,8 +59,9 @@
 
     <?php
     function this_id_actually_exists($id) {
-        echo '<h3>'.$products[$id].'</h3>';
-        if($products[$id] != null){
+        echo '<h3>'.$products[$id]['OID'].'</h3>';
+        echo '<h3>'.count($products[$id]).'</h3>';
+        if(count($products[$id]) != 0){
             return true;
         }else {
             return false;
