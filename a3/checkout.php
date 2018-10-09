f48a9cc7ae878a0ebfd4076b6474cc7a1d1172bf
julia98 <925583000@qq.com>
2018-10-09 15:57:34 +1100

    fix

--- a3/checkout.php
+++ a3/checkout.php
@@ -92,8 +92,8 @@
                 </div>
                 <div class="six columns">
                     <label for="exampleEmailInput">Credit Card</label>
-                    <input class="u-full-width" type="text" id="credit_card" name="credit_card" oninput="validate_cardNumber()" style="float:left">
-                    <img src="images/visa.png" id="visaIcon" style="display:none" style="float:left" />
+                    <input class="eighty-width" type="text" id="credit_card" name="credit_card" oninput="validate_cardNumber()" style="float:left">
+                    <img src="images/visa.png" class="twenty-width" id="visaIcon" style="display:none" />
                 </div>
                 <div class="six columns">
                     <label for="exampleEmailInput">Expiry Date</label>
