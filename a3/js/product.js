c06d3eab02b4a0ad50dea9304dfa1f8159118a35
julia98 <925583000@qq.com>
2018-10-11 02:22:34 +1100

    fix

--- a3/js/product.js
+++ a3/js/product.js
@@ -24,10 +24,10 @@ function changeSubtotal(){
     var qty = document.getElementById("qty").value;
     qty = parseFloat(qty);
     var price = String(document.getElementById("price").innerText);
-    alert("price: " + price);
+    // alert("price: " + price);
     price = parseFloat(price.substr(1));
     var subtotal = qty*price;
     subtotal = subtotal.toFixed(2);
-    alert(subtotal);
-    document.getElementById("subtotal").value = "$" + subtotal;
+    // alert(subtotal);
+    document.getElementById("subtotal").innerText = "$" + subtotal;
 }
